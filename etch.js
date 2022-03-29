bb3e0d42203ba395f7c0ed52eeca26c6acf6dcfa
jkwaser <jkwaser@gmail.com>
2022-03-29 22:28:00 +1000

    Added code to make the div change color on mouseover.

--- etch.js
+++ etch.js
@@ -2,6 +2,9 @@
         const content = document.createElement('div');
         content.classList.add('content');
         content.className = "container-item";
+        content.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'blue';
+            });        
         container.appendChild(content);
     }
 
