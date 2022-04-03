8186adb001041c032553b45ba5617c93deb2e7a4
jkwaser <jkwaser@gmail.com>
2022-04-03 20:21:52 +1000

    Completed etch-a-sketch project.

--- etch.js
+++ etch.js
@@ -1,10 +1,35 @@
-    for (let i = 0; i < 250; i++) { 
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.className = "container-item";
-        content.addEventListener('mouseover', function (e) {
-            e.target.style.background = 'blue';
-            });        
-        container.appendChild(content);
+function createEtch(sideLength) {
+    if (sideLength > 64) {
+        sideLength = 64;
+    } 
+    for (let i = 0; i < sideLength; i++) { 
+        let row = document.createElement("div");
+        row.className = "row"; 
+        for(let j = 1; j <= sideLength; j++){    
+            let cell = document.createElement("div"); 
+            cell.className = "square"; 
+            cell.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'green';
+                });       
+            row.appendChild(cell); 
+
+        }
+        container.appendChild(row); 
     }
+}
+
+createEtch(10);
+
+const reset= document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+    row.remove();
+    });
+   let squareHeight = prompt("How many rows do you want for this new Etch-A-Sketch?", 10);
+    createEtch(squareHeight);
+
+   
+});
+
 
